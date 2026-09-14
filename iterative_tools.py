ca98bc826f22e2639146fa1a32d2aea4cc893268
Myra Gull <myragull749@gmail.com>
2026-09-14 10:17:21 +0500

    build state of iterartive workflows

--- iterative_tools.py
+++ iterative_tools.py
@@ -26,3 +26,20 @@ writer_llm_with_tools = writer_llm.bind_tools(tools)
 # reviewer
 
 reviewer_llm = ChatGroq(model="openai/gpt-oss-120b",temperature=0.1)
+
+
+#state building
+
+class State(TypedDict):
+    topic : str
+    # the single topic received by the writer llm
+    messages : Annotated[list,add_messages]
+    # for maintianing the conversation history acorss llms with api keys
+    draft : str
+    # not an original post a draft to be revied again and again
+    review_feedback : str
+    # the feedback received by the witer llm again and again
+    is_approved :bool
+    # whether the draft is approved or not by the reviewer llm
+    attempts : int
+    # how many attempts
